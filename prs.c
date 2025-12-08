cdc233c7afc149e514d30bfa87c2c98b3df69536
Vasili Vasilyeu <vasili_vasilyeu@epam.com>
2025-12-07 21:39:49 -0800

    Implement pressure (and temperature from pressure sensor) measurement procedure

--- prs.c
+++ prs.c
@@ -72,6 +72,26 @@ int prs_wait_coefs(uint8_t addr, bool *rdy)
 	return PICO_OK;
 }
 
+int16_t _u12_to_s(uint16_t u)
+{
+	return (u & 0x0800)?  -2048 + (u & 0x07ff) : u;
+}
+
+int16_t _u16_to_s(uint16_t u)
+{
+	return (u & 0x8000)? -32768 + (u & 0x7fff) : u;
+}
+
+int32_t _u20_to_s(uint32_t u)
+{
+	return (u & 0x00080000)?  -524288 + (u & 0x0007ffff) : u;
+}
+
+int32_t _u24_to_s(uint32_t u)
+{
+	return (u & 0x00800000)? -8388608 + (u & 0x007fffff) : u;
+}
+
 int prs_read_coefs(uint8_t addr, prs_coefs_t *coefs)
 {
 	uint8_t raw_coefs[PRS_COEF_SIZE];
@@ -82,17 +102,108 @@ int prs_read_coefs(uint8_t addr, prs_coefs_t *coefs)
 		return r;
 	}
 
-	coefs->c0 = ((short)raw_coefs[0] << 4) | ((short)raw_coefs[1] >> 4);
-	coefs->c1 = (((short)raw_coefs[1] & 0xf) << 8) | (short)raw_coefs[2];
-	coefs->c00 = ((int)raw_coefs[3] << 12) | ((int)raw_coefs[4] << 4) | ((int)raw_coefs[5] >> 4);
-	coefs->c10 = (((int)raw_coefs[5] & 0xf) << 16) | ((int)raw_coefs[6] << 8) | (int)raw_coefs[7];
-	coefs->c01 = ((short)raw_coefs[8] << 8) | (short)raw_coefs[9];
-	coefs->c11 = ((short)raw_coefs[10] << 8) | (short)raw_coefs[11];
-	coefs->c20 = ((short)raw_coefs[12] << 8) | (short)raw_coefs[13];
-	coefs->c21 = ((short)raw_coefs[14] << 8) | (short)raw_coefs[15];
-	coefs->c30 = ((short)raw_coefs[16] << 8) | (short)raw_coefs[17];
-	coefs->c31 = ((short)raw_coefs[18] << 4) | ((short)raw_coefs[19] >> 4);
-	coefs->c40 = (((short)raw_coefs[19] & 0xf) << 8) | (short)raw_coefs[20];
+	coefs->c0 = _u12_to_s(((uint16_t)raw_coefs[0] << 4) | ((uint16_t)raw_coefs[1] >> 4));
+	coefs->c1 = _u12_to_s((((uint16_t)raw_coefs[1] & 0xf) << 8) | (uint16_t)raw_coefs[2]);
+	coefs->c00 = _u20_to_s(((uint32_t)raw_coefs[3] << 12) |
+	                       ((uint32_t)raw_coefs[4] << 4) |
+	                       ((uint32_t)raw_coefs[5] >> 4));
+	coefs->c10 = _u20_to_s((((uint32_t)raw_coefs[5] & 0xf) << 16) |
+	                       ((uint32_t)raw_coefs[6] << 8) |
+	                       (uint32_t)raw_coefs[7]);
+	coefs->c01 = _u16_to_s(((uint16_t)raw_coefs[8] << 8) | (uint16_t)raw_coefs[9]);
+	coefs->c11 = _u16_to_s(((uint16_t)raw_coefs[10] << 8) | (uint16_t)raw_coefs[11]);
+	coefs->c20 = _u16_to_s(((uint16_t)raw_coefs[12] << 8) | (uint16_t)raw_coefs[13]);
+	coefs->c21 = _u16_to_s(((uint16_t)raw_coefs[14] << 8) | (uint16_t)raw_coefs[15]);
+	coefs->c30 = _u16_to_s(((uint16_t)raw_coefs[16] << 8) | (uint16_t)raw_coefs[17]);
+	coefs->c31 = _u12_to_s(((uint16_t)raw_coefs[18] << 4) | ((uint16_t)raw_coefs[19] >> 4));
+	coefs->c40 = _u12_to_s((((uint16_t)raw_coefs[19] & 0xf) << 8) | (uint16_t)raw_coefs[20]);
 
 	return PICO_OK;
+}
+
+int32_t prs_prc_to_k(uint8_t prc)
+{
+	switch (prc)
+	{
+	case PRS_PRC_1:
+		return 524288;  //0x00080000
+	case PRS_PRC_2:
+		return 1572864; //0x00180000
+	case PRS_PRC_4:
+		return 3670016; //0x00380000
+	case PRS_PRC_8:
+		return 7864320; //0x00780000
+	case PRS_PRC_16:
+		return 253952;  //0x0003e000
+	case PRS_PRC_32:
+		return 516096;  //0x0007e000
+	case PRS_PRC_64:
+		return 1040384; //0x000fe000
+	case PRS_PRC_128:
+		return 2088960; //0x001fe000
+	}
+
+	return -1;
+}
+
+int prs_config(uint8_t addr, uint8_t pm_prc, uint8_t tmp_prc)
+{
+	int r = bus_write_byte(addr, PRS_PRS_CFG, PRS_RATE_1_MPS | (pm_prc & PRS_PRC_MASK));
+	if (r != PICO_OK)
+	{
+		return r;
+	}
+
+	r = bus_write_byte(addr, PRS_TMP_CFG, PRS_RATE_1_MPS | (tmp_prc & PRS_PRC_MASK));
+	if (r != PICO_OK)
+	{
+		return r;
+	}
+
+	uint8_t cfg = PRS_CFG_INT_PRS | PRS_CFG_INT_TMP;
+
+	if (pm_prc & PRS_PRC_SHIFT_CHK)
+	{
+		cfg |= PRS_CFG_P_SHIFT;
+	}
+
+	if (tmp_prc & PRS_PRC_SHIFT_CHK)
+	{
+		cfg |= PRS_CFG_T_SHIFT;
+	}
+
+	return bus_write_byte(addr, PRS_CFG_REG, cfg);
+}
+
+int prs_meas_config(uint8_t addr)
+{
+	return bus_write_byte(addr, PRS_MEAS_CFG, PRS_MEAS_CTRL_CPT);
+}
+
+int prs_meas_stop(uint8_t addr)
+{
+	return bus_write_byte(addr, PRS_MEAS_CFG, 0);
+}
+
+int _get_raw(uint8_t addr, uint8_t reg, int32_t *value)
+{
+	uint8_t data[3];
+	int r = bus_read_block(addr, reg, data, sizeof(data)/sizeof(data[0]));
+	if (r != PICO_OK)
+	{
+		return r;
+	}
+
+	*value = _u24_to_s(((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | (uint32_t)data[2]);
+	return PICO_OK;
+}
+
+int prs_get_prs_raw(uint8_t addr, int32_t *prs)
+{
+	return _get_raw(addr, PRS_PRS, prs);
+}
+
+int prs_get_tmp_raw(uint8_t addr, int32_t *tmp)
+{
+	return _get_raw(addr, PRS_TMP, tmp);
 }
--- prs.h
+++ prs.h
@@ -4,11 +4,72 @@
 #define PRS_ADDR     0x77
 #define PRS_ADDR_ALT 0x76
 
+#define PRS_PRS      0x00
+#define PRS_TMP      0x03
+#define PRS_PRS_CFG  0x06
+#define PRS_TMP_CFG  0x07
 #define PRS_MEAS_CFG 0x08
+#define PRS_CFG_REG  0x09
+#define PRS_INT_STS  0x0A
 #define PRS_REG_ID   0x0D
 #define RRS_COEF     0x10
 
+#define PRS_RATE_MASK 0x0f
+#define PRS_PRC_MASK  0x07
+
+#define PRS_RATE_1_MPS     0x00
+#define PRS_RATE_2_MPS     0x10
+#define PRS_RATE_4_MPS     0x20
+#define PRS_RATE_8_MPS     0x30
+#define PRS_RATE_16_MPS    0x40
+#define PRS_RATE_32_MPS    0x50
+#define PRS_RATE_64_MPS    0x60
+#define PRS_RATE_128_MPS   0x70
+#define PRS_RATE_25_16_MPS 0x80
+#define PRS_RATE_25_8_MPS  0x90
+#define PRS_RATE_25_4_MPS  0xa0
+#define PRS_RATE_25_2_MPS  0xb0
+#define PRS_RATE_25_MPS    0xc0
+#define PRS_RATE_50_MPS    0xd0
+#define PRS_RATE_100_MPS   0xe0
+#define PRS_RATE_200_MPS   0xf0
+
+#define PRS_PRC_1   0x00
+#define PRS_PRC_2   0x01
+#define PRS_PRC_4   0x02
+#define PRS_PRC_8   0x03
+#define PRS_PRC_16  0x04
+#define PRS_PRC_32  0x05
+#define PRS_PRC_64  0x06
+#define PRS_PRC_128 0x07
+
+#define PRS_PRS_RDY  0x10
+#define PRS_TMP_RDY  0x20
 #define PRS_COEF_RDY 0x80
+
+#define PRS_MEAS_CTRL_MASK 0x07
+
+#define PRS_MEAS_CTRL_IDLE 0
+#define PRS_MEAS_CTRL_P    1
+#define PRS_MEAS_CTRL_T    2
+#define PRS_MEAS_CTRL_CP   5
+#define PRS_MEAS_CTRL_CT   6
+#define PRS_MEAS_CTRL_CPT  7
+
+#define PRS_PRC_SHIFT_CHK 0x04
+
+#define PRS_CFG_SPI_MODE 0x01
+#define PRS_CFG_FIFO_EN  0x02
+#define PRS_CFG_P_SHIFT  0x04
+#define PRS_CFG_T_SHIFT  0x08
+#define PRS_CFG_INT_PRS  0x10
+#define PRS_CFG_INT_TMP  0x20
+#define PRS_CFG_INT_FIFO 0x40
+#define PRS_CFG_INT_HL   0x80
+
+#define PRS_INT_PRS       0x01
+#define PRS_INT_TMP       0x02
+#define PRS_INT_FIFO_FULL 0x04
 
 #define PRS_ID 0x11
 
@@ -16,17 +77,17 @@
 #define PRS_COEF_SIZE 21
 
 typedef struct _prs_coefs {
-	short c0;
-	short c1;
-	int c00;
-	int c10;
-	short c01;
-	short c11;
-	short c20;
-	short c21;
-	short c30;
-	short c31;
-	short c40;
+	int16_t c0;
+	int16_t c1;
+	int32_t c00;
+	int32_t c10;
+	int16_t c01;
+	int16_t c11;
+	int16_t c20;
+	int16_t c21;
+	int16_t c30;
+	int16_t c31;
+	int16_t c40;
 } prs_coefs_t;
 
 int prs_find(uint8_t *addr);
@@ -34,5 +95,12 @@ int prs_get_id(uint8_t addr, uint8_t *id);
 int prs_check_coefs(uint8_t addr, bool *rdy);
 int prs_wait_coefs(uint8_t addr, bool *rdy);
 int prs_read_coefs(uint8_t addr, prs_coefs_t *coefs);
+int prs_config(uint8_t addr, uint8_t pm_prc, uint8_t tmp_prc);
+int prs_meas_config(uint8_t addr);
+int prs_meas_stop(uint8_t addr);
+int prs_get_prs_raw(uint8_t addr, int32_t *prs);
+int prs_get_tmp_raw(uint8_t addr, int32_t *tmp);
+
+int32_t prs_prc_to_k(uint8_t prc);
 
 #endif //PRS_H
